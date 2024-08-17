8d5308bb3859650a9cf37d01b8c9904930de78be
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-17 15:39:09 +0100

    finalizando o projecto

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,117 @@ import { BannerMoreWeek } from "./components/bannerMoreWeek";
 import { BannerMoreWeekLeft } from "./components/bannerMoreWeekLeft";
 import { BannerWelcome } from "./components/bannerWelcome";
 import { Header } from "./components/header";
+import Ads from "../public/Ads.png";
+import Imagem14 from "../public/Image14.png";
+import Imagem15 from "../public/Image15.png";
+import Imagem16 from "../public/Image16.png";
+import Imagem17 from "../public/Image17.png";
+import Imagem18 from "../public/Image18.png";
+import { Separator } from "./components/separator";
+import { ArrowRight } from "lucide-react";
+import { TitleArtigo } from "./components/titleArtigo";
 
 export function App() {
   return (
-    <div>
+    <div className="pb-8">
       <Header />
       <main className="px-8 space-y-20">
         <BannerWelcome />
         <BannerMoreWeek />
-        <div className="space-x-8">
+        <div className="flex gap-8 justify-center">
           <BannerMoreWeekLeft />
-          <div></div>
+          <div className="space-y-8">
+            <figure className="">
+              <img
+                src={Ads}
+                alt="publicidade rocketseat"
+                className="w-full h-[280px] rounded"
+              />
+            </figure>
+            <div className="space-y-6">
+              <div>
+                <Separator />
+                <div className="flex justify-between pt-3 text-[#CBD5E1] font-semibold text-sm">
+                  <span>Viu isso aqui?</span>
+                  <button className="flex items-center gap-1 hover:text-[#1D4ED8]">
+                    Ver tudo
+                    <ArrowRight className="size-4" />
+                  </button>
+                </div>
+              </div>
+              <div className="space-y-6">
+                <div className="flex gap-4">
+                  <figure className="size-16">
+                    <img src={Imagem14} alt="" />
+                  </figure>
+                  <div className="space-y-2">
+                    <TitleArtigo>Software</TitleArtigo>
+                    <p className="font-black text-sm truncate w-[488px]">
+                      Aplicativo de monitoramento ambiental ganha
+                      <br /> destaque, incentivando práticas ecológicas e
+                      promovendo a conscientização.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex gap-4">
+                  <figure className="size-16">
+                    <img src={Imagem15} alt="" />
+                  </figure>
+                  <div className="space-y-2">
+                    <TitleArtigo>Software</TitleArtigo>
+                    <p className="font-black text-sm truncate w-[488px]">
+                      Aplicativo de monitoramento ambiental ganha
+                      <br /> destaque, incentivando práticas ecológicas e
+                      promovendo a conscientização.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex gap-4">
+                  <figure className="size-16">
+                    <img src={Imagem16} alt="" />
+                  </figure>
+                  <div className="space-y-2">
+                    <TitleArtigo>Software</TitleArtigo>
+                    <p className="font-black text-sm truncate w-[488px]">
+                      Aplicativo de monitoramento ambiental ganha
+                      <br /> destaque, incentivando práticas ecológicas e
+                      promovendo a conscientização.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex gap-4">
+                  <figure className="size-16">
+                    <img src={Imagem17} alt="" />
+                  </figure>
+                  <div className="space-y-2">
+                    <TitleArtigo>Software</TitleArtigo>
+                    <p className="font-black text-sm truncate w-[488px]">
+                      Aplicativo de monitoramento ambiental ganha
+                      <br /> destaque, incentivando práticas ecológicas e
+                      promovendo a conscientização.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="flex gap-4">
+                  <figure className="size-16">
+                    <img src={Imagem18} alt="" />
+                  </figure>
+                  <div className="space-y-2">
+                    <TitleArtigo>Software</TitleArtigo>
+                    <p className="font-black text-sm truncate w-[488px]">
+                      Aplicativo de monitoramento ambiental ganha
+                      <br /> destaque, incentivando práticas ecológicas e
+                      promovendo a conscientização.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
--- src/components/bannerMoreWeekLeft.tsx
+++ src/components/bannerMoreWeekLeft.tsx
@@ -9,7 +9,7 @@ import Imagem13 from "../../public/Image13.png";
 export function BannerMoreWeekLeft() {
   return (
     <div className="space-y-6 w-[696px]">
-      <div className="pt-3">
+      <div>
         <Separator />
         <div className="flex justify-between py-2">
           <span className="font-semibold text-sm text-[#CBD5E1]">
@@ -47,17 +47,21 @@ export function BannerMoreWeekLeft() {
         <div className="space-y-2 w-[504px]">
           <TitleArtigo>Inteligência Artificial</TitleArtigo>
           <h2 className="font-extrabold truncate">
-            Empresa surpreende o mundo ao anunciar um algoritmo capaz
-            <br /> de prever eventos futuros com alta precisão.
+            Dispositivo portátil promete traduzir instantaneamente
+            <br /> diferentes idiomas, facilitando a comunicação global.
           </h2>
           <p className="text-sm text-[#CBD5E1] truncate">
-            Em um avanço surpreendente da inteligência artificial, uma empresa
-            anuncia o<br /> desenvolvimento de um algoritmo capaz de prever
-            eventos futuros com notável
-            <br /> precisão. Descubra como essa tecnologia está desafiando as
-            fronteiras da
-            <br /> previsão e quais são as implicações para diversas áreas,
-            desde finanças até planejamento estratégico.
+            Em um mundo cada vez mais conectado, a comunicação sem fronteiras é
+            <br /> essencial. Nesse contexto, um dispositivo portátil surge como
+            um verdadeiro
+            <br /> herói linguístico, prometendo quebrar as barreiras
+            idiomáticas instantaneamente.
+            <br /> Imagine poder se comunicar fluentemente em qualquer lugar do
+            mundo, independentemente do idioma local. Este dispositivo não é
+            mais uma visão futurista, mas uma realidade impressionante que está
+            transformando a forma como nos conectamos globalmente. Nesta
+            reportagem, mergulharemos nas funcionalidades deste dispositivo
+            revolucionário e como ele está moldando uma
           </p>
         </div>
         <div className="size-44">
@@ -69,17 +73,23 @@ export function BannerMoreWeekLeft() {
         <div className="space-y-2 w-[504px]">
           <TitleArtigo>Inteligência Artificial</TitleArtigo>
           <h2 className="font-extrabold truncate">
-            Empresa surpreende o mundo ao anunciar um algoritmo capaz
-            <br /> de prever eventos futuros com alta precisão.
+            Criados para auxiliar idosos, robôs de companhia ganham
+            <br /> popularidade, oferecendo suporte emocional e físico aos
+            usuários.
           </h2>
           <p className="text-sm text-[#CBD5E1] truncate">
-            Em um avanço surpreendente da inteligência artificial, uma empresa
-            anuncia o<br /> desenvolvimento de um algoritmo capaz de prever
-            eventos futuros com notável
-            <br /> precisão. Descubra como essa tecnologia está desafiando as
-            fronteiras da
-            <br /> previsão e quais são as implicações para diversas áreas,
-            desde finanças até planejamento estratégico.
+            A tecnologia não só avança, mas também busca tornar a vida mais
+            significativa
+            <br /> para todas as gerações. No cenário atual, os robôs sociais,
+            projetados
+            <br /> especialmente para oferecer apoio e companhia aos idosos,
+            estão ganhando
+            <br /> destaque. Mais do que meros assistentes, esses robôs são
+            programados para fornecer suporte emocional e físico, preenchendo
+            lacunas nas necessidades de cuidado. Nesta matéria, exploraremos
+            como esses companheiros tecnológicos estão impactando positivamente
+            a qualidade de vida da terceira idade e proporcionando uma nova
+            perspectiva sobre o envelhecimento.
           </p>
         </div>
         <div className="size-44">
@@ -91,17 +101,25 @@ export function BannerMoreWeekLeft() {
         <div className="space-y-2 w-[504px]">
           <TitleArtigo>Inteligência Artificial</TitleArtigo>
           <h2 className="font-extrabold truncate">
-            Empresa surpreende o mundo ao anunciar um algoritmo capaz
-            <br /> de prever eventos futuros com alta precisão.
+            Aplicativo utiliza inteligência artificial para ajudar usuários a
+            <br />
+            manterem o foco e aumentarem a produtividade no dia a dia.
           </h2>
           <p className="text-sm text-[#CBD5E1] truncate">
-            Em um avanço surpreendente da inteligência artificial, uma empresa
-            anuncia o<br /> desenvolvimento de um algoritmo capaz de prever
-            eventos futuros com notável
-            <br /> precisão. Descubra como essa tecnologia está desafiando as
-            fronteiras da
-            <br /> previsão e quais são as implicações para diversas áreas,
-            desde finanças até planejamento estratégico.
+            No turbilhão da vida moderna, onde distrações são abundantes, um
+            aplicativo
+            <br /> inovador surge como um aliado indispensável para quem busca
+            maior
+            <br /> concentração e eficiência no cotidiano. Utilizando avançados
+            algoritmos de
+            <br /> inteligência artificial, este aplicativo promete mais do que
+            simplesmente gerenciar tarefas; ele está programado para compreender
+            os padrões de trabalho de cada usuário, oferecendo sugestões
+            personalizadas para otimizar o tempo e maximizar a produtividade.
+            Nesta matéria, exploraremos como essa ferramenta tecnológica está se
+            tornando a chave para manter o foco em um mundo cada vez mais cheio
+            de distrações, transformando a maneira como encaramos nossas
+            responsabilidades diárias.
           </p>
         </div>
         <div className="size-44">
